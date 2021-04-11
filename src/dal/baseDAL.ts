13a609abded4be9af8d57bee9bc94c999ecca26d
Gal Blond <318639119@idf.il>
2021-04-11 22:49:36 +0300

    Fixing Teacher creation

--- src/dal/baseDAL.ts
+++ src/dal/baseDAL.ts
@@ -11,7 +11,7 @@ export class BaseDataAccess<T extends Document> implements IBaseDataAccess {
   }
 
   public async create(document: T) {
-    this.model.create(document);
+    return await this.model.create(document);
   }
 
   public async findById(id: String) {
